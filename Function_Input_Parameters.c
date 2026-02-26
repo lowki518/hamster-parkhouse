5e789c16fd5a0ca4b5c2ddc248a04fa83f16475f
paminalessle <paminalessle@gmail.com>
2026-02-26 18:22:09 +0000

    Function prototypes besides Programm_Loop and Function_statistics

--- Function_Input_Parameters.c
+++ Function_Input_Parameters.c
@@ -1,14 +1,17 @@
 /*
-FUNCTION imput_parameters ()
-    PRINT overview of parameters
+FUNCTION imput_parameters (overview of parameters)
+    PRINT (overview of parameters)
 
-    INPUT (parameter to change)
-    
-    IF (value input correct) THEN
-        PRINT ("Done?")
-    ELSE 
-        Print ("Incorrect Input")
-    END IF
+    WHILE (TRUE)
+        INPUT (parameter to change)
 
-
+        IF (value input correct) THEN
+            PRINT ("Done?")
+            IF (TRUE) THEN
+                RETURN
+            ELSE DO
+                Print ("Incorrect Input")
+            END IF
+        END IF
+    END WHILE
 */
---
+++ Function_statistics.c
@@ -0,0 +1,6 @@
+/*
+FUNCTION statistics()
+    
+
+
+*/
