7216c7394436b07afa2a9760fb5bf040f6f82c6c
paminalessle <paminalessle@gmail.com>
2026-03-02 15:08:35 +0000

    Final edits

--- README.md
+++ README.md
@@ -1,39 +1,15 @@
 # Plan
-1. I/O Modules for configurations
-2. Simulation:
-    2.1 Data types for stuff and things
-    2.2 Functions for simulation steps
-3. Statistics Output
-    3.1 CLI (Command Line Interface)
-    3.2 Textfile
+1. I/O Modules for configurations [(link to docs)](doc/cli.md)
+ - CLI to choose config
+2. Simulation
+ - Simulate data and output per timestep
+3. Statistics Outputgit 
+- open GUI with data
 
 ## TODO: 
-- Make github repo
-- Make Header (.h) and first code (.c) files
-- Plan Statistics
-- CLI planning
-- Data Type planning
-- function prototypes
-- pseudocode (**** this)
-- Task splitting threeways
-- Documentation of everything!!!!!!!!!!!!!111!!11!!1!!!!
-
+- Uniform naming
+- Part II (to be continued)
 
 The minmal amount of timesteps needs to be 4, or else the visualization breaks
 
-
-```main.c:
-#include things
-#include stuff
-#include whatever
-
-int main(){
-    do_cli_for_settings();
-
-    do_simulations()
-
-
-    do_statistics();
-
-}
-```
+This is only part I of the given task, so nothing compiles or works, so thers nothing to instruct yet.
--- src/cli/cli.h
+++ src/cli/cli.h
@@ -1,46 +1,49 @@
 #ifndef CLI 
 #define CLI
 
-/**
- * @brief Read user input and error handling for bad input
- * 
- * @param[in] p_input pointer on input string (char *) 
- * @return 0 if successful, otherwise 1
+/*
+  @brief Read user input and error handling for bad input
+  
+  @param[1] p_input A pointer on input string (char *) 
+ 
+  @return 0 if successful, otherwise 1
  */
 int read_user_input(char * input);
 
 
-/**
- * @brief Handle user input commands
- * 
- * @param[in] p_input pointer on input string (char *) 
- * @return 0 if successful, otherwise 1
+/*
+  @brief Handle user input commands
+  
+  @param[1] p_input A pointer on input string (char *) 
+ 
+  @return 0 if successful, otherwise 1
  */
 int handle_user_input(char * input);
 
 
-/** 
- * @brief Print command overview
+/*
+  @brief Print start menu (UI) and wait for user input
+
+  @return void
  */
 void start_menu();
 
 
-/**
- * @brief Print configs + value
- * 
- * @param p_<config_variable> Pointer to config variable 
- * ...
+/* 
+  @brief Print command overview
+ 
+  @return void
  */
 void print_help();
 
+/*
+  @brief Print configs + value
+ 
+  @param[1] p_<config_variable> A pointer to config variable 
+  ...
 
-/**
- * @brief Print start menu (UI) and wait for user input
+  @return void
  */
 void print_config();
-
-
-
-
 
 #endif 
