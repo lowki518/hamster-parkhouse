d77bd8c0887b06c32c9305e15c3230ded1cc2ed1
paminalessle <paminalessle@gmail.com>
2026-03-02 12:39:03 +0000

    Final changes

--- Function_Time_Step.c
+++
@@ -1,7 +0,0 @@
-/*
-FUNCTION time_step ()
-    FUNCTION check_parked_cars
-    FUNCTION check_queue
-    FUNCTION car_arrival
-    FUNCTION statistics
-*/
--- lib/sim_lib.h
+++
@@ -1,7 +0,0 @@
-#ifndef SIM_LIB_H
-#define SIM_LIB_H
-
-#include "data_types.h"
-
-
-#endif
