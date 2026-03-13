e78535ccab563618df5183db9fdab86da9d031d9
paminalessle <paminalessle@gmail.com>
2026-03-13 09:36:45 +0000

    small edits to unit tests and file file_manager

--- test/file_manager_test.c
+++ test/file_manager_test.c
@@ -11,13 +11,4 @@ void test_get_file_number() {
 
     assert(get_new_file_number("./data_test_1") == 3);
     assert(get_new_file_number("./data_test_2") == 1);
-
 }
-
-/*
-@brief tests the create_new_file_with_head_data function
-*/
-void test_create_new_file_with_head_data (int new_file_number, t_Time sim_duration, int parking_cells, t_Time max_parking_duration, float new_car_prob, int max_cars_arriving, unsigned int seed) {
-    create_new_file_with_head_data()
-}
- 
